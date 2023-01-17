d5628d52eb35499b3746ad7b59710f7227318f2f
panupong <panuponglovecal@gmail.com>
2023-01-17 17:07:12 +0700

    today

--- first-jobber-app/src/App.js
+++ first-jobber-app/src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
+import Header from './components/Header/Header'
 import Signup from './components/Signup/Signup';
 import Login from './components/Login/Login';
+import Questionnaire from "./components/Questionnaire/Questionnaire";
 
 const App = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Header />
+            <Home />
+          </>
+        }
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/questionare" element={<Questionnaire />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- first-jobber-app/src/components/Home/Home.js
+++ first-jobber-app/src/components/Home/Home.js
@@ -6,19 +6,19 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 const Home = () => {
     return (
       <div className="home-page">
-        <Navbar bg="light" expand="lg" fixed="top">
+        {/* <Navbar bg="light" expand="lg" fixed="top">
           <Navbar.Brand>First Jobber Choice</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
               <Nav.Link as={Link} to="/signup" className="mr-3">Sign Up</Nav.Link>
               <Nav.Link as={Link} to="/login" className="mr-3">Log In</Nav.Link>
+              <Nav.Link as={Link} to="/questionare" className="mr-3">Test</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Container className="mt-5">
-        <h1 className="display-4 text-center">Welcome to First Jobber Choice</h1>
-        </Container>
+        <Container className="mt-5 text-center">
+          <h1 className="display-4">Welcome to First Jobber Choice</h1>
+        </Container> */}
       </div>
     );
   };
