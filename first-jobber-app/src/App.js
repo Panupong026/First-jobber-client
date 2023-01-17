511c146e0ac5246afee1c6f58f7136b8107479ab
panupong <panuponglovecal@gmail.com>
2023-01-17 13:22:38 +0700

    signup

--- first-jobber-app/src/App.js
+++ first-jobber-app/src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
+import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
+    <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
--- first-jobber-app/src/components/Home/Home.js
+++ first-jobber-app/src/components/Home/Home.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Home = () => {
     return (
       <div className="home-page">
-        <nav className="navbar">
-        </nav>
-        <div className="welcome-message">
-          <h1>Welcome to first jobber choice</h1>
-        </div>
+        <Navbar bg="light" expand="lg" fixed="top">
+          <Navbar.Brand>First Jobber Choice</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link as={Link} to="/signup" className="mr-3">Sign Up</Nav.Link>
+              <Nav.Link as={Link} to="/login" className="mr-3">Log In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Container className="mt-5">
+        <h1 className="display-4 text-center">Welcome to First Jobber Choice</h1>
+        </Container>
       </div>
     );
   };
-  
 
 export default Home;
