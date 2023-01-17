b3c023e41c2fd01a21805b7a5e344bb242816e9f
panupong <panuponglovecal@gmail.com>
2023-01-17 12:11:25 +0700

    env

---
+++ first-jobber-app/src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+const Home = () => {
+    return (
+      <div className="home-page">
+        <nav className="navbar">
+        </nav>
+        <div className="welcome-message">
+          <h1>Welcome to first jobber choice</h1>
+        </div>
+      </div>
+    );
+  };
+  
+
+export default Home;
