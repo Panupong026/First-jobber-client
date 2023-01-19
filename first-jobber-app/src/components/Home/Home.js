dcc94aeb2ccf36161bbfa89ab726619ffed2fbaa
panupong <panuponglovecal@gmail.com>
2023-01-19 12:27:16 +0700

    display database

--- first-jobber-app/src/components/Home/Home.js
+++ first-jobber-app/src/components/Home/Home.js
@@ -8,6 +8,11 @@ const Home = () => {
       <div className="home-page">
         <Container className="mt-5 text-center">
           <h1 className="display-4">Welcome to First Jobber Choice</h1>
+          <Navbar.Collapse className="navbar-nav" id="basic-navbar-nav">
+      <Nav className="ml-auto normal-link">
+        <Nav.Link as={Link} to="/questionare" className="mr-3 nav-item">Test</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
         </Container>
       </div>
     );
